c78a81cd7c4801d5e44d634574042d9b8edb71f3
Julia Reichel <jrr2189@barnard.edu>
2020-06-01 10:48:48 -0400

    Implementing fetch().

--- portfolio/src/main/webapp/home.js
+++ portfolio/src/main/webapp/home.js
@@ -1,3 +1,18 @@
+function getGreeting() {
+    console.log("in the function")
+    fetch('/data').then(response => response.text()).then((greeting)=> {
+        console.log("fetching the greeting")
+        document.getElementById('greeting_container').innerText = greeting;
+        console.log("should of printed!")
+    });
+}
+
+async function getGreetingAsyncAwait() {
+  const response = await fetch('/data');
+  const quote = await response.text();
+  document.getElementById('greeting_container').innerText = quote;
+}
+
 function open_linkedin() {
     window.open("https://www.linkedin.com/in/juliareichel/");
 }
