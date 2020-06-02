ee62cb469370506f6c29afc32d65bda05d22212b
Julia Reichel <jrr2189@barnard.edu>
2020-06-02 16:55:11 +0000

    Implemented datastore and loading.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,8 +14,14 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
 import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,20 +39,18 @@ public class DataServlet extends HttpServlet {
   }
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("application/json;");
-    String json = new Gson().toJson(emails);
-    response.getWriter().println(json);
-  }
-
-  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       String email = getEmail(request);
+      long timestamp = System.currentTimeMillis();
 
+      Entity emailEntity = new Entity("Email");
       if (email != "") {
-        emails.add(email);
+        emailEntity.setProperty("email", email);
+        emailEntity.setProperty("timestamp", timestamp);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(emailEntity);
       }
-
+      
       response.setContentType("text/html");
       response.sendRedirect("/index.html");
   }
@@ -58,5 +62,26 @@ public class DataServlet extends HttpServlet {
         return value;
     }
     return "";
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Email").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      String email = (String) entity.getProperty("email");
+      long timestamp = (long) entity.getProperty("timestamp");
+
+      if (!emails.contains(email)){
+        emails.add(email);  
+      }
+    }
+
+    response.setContentType("application/json;");
+    String json = new Gson().toJson(emails);
+    response.getWriter().println(json);
   }
 }
