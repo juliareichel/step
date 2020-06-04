ae9dc1aaee0128de616a3ba7b07060492a54353e
Julia Reichel <jrr2189@barnard.edu>
2020-06-04 22:49:22 +0000

    Separated functions into smaller functions.

--- portfolio/src/main/webapp/home.js
+++ portfolio/src/main/webapp/home.js
@@ -1,37 +1,47 @@
 function getContact() {
     var quantity = getLimit();
     fetch('/data?quantity='+ quantity).then(response => response.json()).then((emails) => {
-        emails.forEach(email => {
-          const singleEmailDiv = document.createElement('div');
-          singleEmailDiv.classList.add("email_div_spacing");
-          singleEmailDiv.innerText = email;
-          document.getElementById('email_list').appendChild(singleEmailDiv);
-          const replyButton = document.createElement('button');
-          replyButton.innerText = "Reply to this Person";
-          replyButton.classList.add("button_spacing");
-          singleEmailDiv.appendChild(replyButton);
-          const replyForm = document.createElement('form');
-          replyForm.method = 'POST';
-          replyForm.action = '/reply?replyId=${replyId}';
-          singleEmailDiv.appendChild(replyForm);
-
-          replyButton.addEventListener("click", () => {
-            replyButton.classList.add("hide");
-            const replyInput = document.createElement('textarea');
-            replyInput.name = "reply-input";
-            replyForm.appendChild(replyInput);
-            const submitReplyButton = document.createElement('button');
-            submitReplyButton.innerText = "Reply";
-            replyForm.appendChild(submitReplyButton);
-            submitReplyButton.classList.add("button_spacing");
-              submitReplyButton.addEventListener("click", () => {
-                fetch(replyForm.action).then(response => response.json().then((replies) => {
-                  //add to proper original comment
-                }))
-              })
-          })
-        })
+        emails.forEach((email) => createEmailNode(email));
     });
+}
+
+function getLimit() {
+    var value = document.getElementById("quantity_value").value;
+    return value;
+}
+
+function createEmailNode(email) {
+  const singleEmailDiv = document.createElement('div');
+  singleEmailDiv.classList.add("email_div_spacing");
+  singleEmailDiv.innerText = email;
+  document.getElementById('email_list').appendChild(singleEmailDiv);
+  const replyButton = document.createElement('button');
+  replyButton.innerText = "Reply to this Person";
+  replyButton.classList.add("button_spacing");
+  singleEmailDiv.appendChild(replyButton);
+  const replyForm = document.createElement('form');
+  replyForm.method = 'POST';
+  replyForm.action = '/reply?replyId=${replyId}';
+  singleEmailDiv.appendChild(replyForm);
+  replyButton.addEventListener("click", () => onReply());  
+}
+
+function onReply() {
+  replyButton.classList.add("hide");
+  const replyInput = document.createElement('textarea');
+  replyInput.name = "reply-input";
+  replyForm.appendChild(replyInput);
+  const submitReplyButton = document.createElement('button');
+  submitReplyButton.innerText = "Reply";
+  replyForm.appendChild(submitReplyButton);
+  submitReplyButton.classList.add("button_spacing");
+  submitReplyButton.addEventListener("click", () => submitReplyButton());
+}
+
+function submitReply() {
+  fetch(replyForm.action).then(response => response.json()).then((replies) => {
+    //add to proper original comment
+  })
 }
 
 //For Reference Purposes:
@@ -41,11 +51,6 @@ function getContact() {
 //   console.log("fetching the list: ", list);
 //   document.getElementById('list_container').innerText = list;
 // }
-
-function getLimit() {
-    var value = document.getElementById("quantity_value").value;
-    return value;
-}
 
 function deleteContact() {
   const request = new Request('/delete-data', {method: 'POST'});
