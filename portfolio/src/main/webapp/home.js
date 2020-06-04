ca6683aabebdc9ab517116545bcd2f705165eba6
Julia Reichel <jrr2189@barnard.edu>
2020-06-04 21:49:06 +0000

    Implemented client-side reply functionality.

--- portfolio/src/main/webapp/home.js
+++ portfolio/src/main/webapp/home.js
@@ -1,7 +1,31 @@
 function getContact() {
     var quantity = getLimit();
     fetch('/data?quantity='+ quantity).then(response => response.json()).then((emails) => {
-        document.getElementById('email_list').innerText = emails.join('\n');
+        // document.getElementById('email_list').innerText = emails.join('\n');
+
+        emails.forEach(email => {
+          var singleEmailDiv = document.createElement('div');
+          singleEmailDiv.classList.add("email_div_spacing");
+          singleEmailDiv.innerText = email;
+          document.getElementById('email_list').appendChild(singleEmailDiv);
+          var replyButton = document.createElement('button');
+          replyButton.innerText = "Reply to this Person";
+          replyButton.classList.add("button_spacing");
+          singleEmailDiv.appendChild(replyButton);
+          var replyForm = document.createElement('form');
+          replyForm.method = 'POST';
+          singleEmailDiv.appendChild(replyForm);
+
+          replyButton.addEventListener("click", () => {
+            replyButton.classList.add("hide");
+            var replyInput = document.createElement('textarea');
+            replyForm.appendChild(replyInput);
+            var submitReplyButton = document.createElement('button');
+            submitReplyButton.innerText = "Reply";
+            replyForm.appendChild(submitReplyButton);
+            submitReplyButton.classList.add("button_spacing");
+          })
+        })
     });
 }
 
