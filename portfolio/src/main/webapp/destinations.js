93792609de6256139d15685b3ac1aab316575d0b
Julia Reichel <jrr2189@barnard.edu>
2020-06-08 20:44:37 +0000

    Changed lets to const.

--- portfolio/src/main/webapp/destinations.js
+++ portfolio/src/main/webapp/destinations.js
@@ -1,29 +1,29 @@
-/** Creates a map and adds it to the page. */
 const curLocation = {lat:40.5500 , lng: 14.2333};
 
+/** Creates a map and adds it to the page. */
 function createMap() {
-  let map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     center: curLocation,
     zoom: 10,
     mapTypeId: 'terrain'
   });
 
-  let marker = new google.maps.Marker({position: curLocation, map: map});
+  const marker = new google.maps.Marker({position: curLocation, map: map});
 
 /** Taken from Google Maps online API*/
-  let searchInput = document.getElementById('search');
-  let searchBox = new google.maps.places.SearchBox(searchInput);
+  const searchInput = document.getElementById('search');
+  const searchBox = new google.maps.places.SearchBox(searchInput);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchInput);
 
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
 
-  let markers = [];
+  const markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
-    let places = searchBox.getPlaces();
+    const places = searchBox.getPlaces();
       if (places.length == 0) {
         return;
       }
@@ -36,7 +36,7 @@ function createMap() {
     markers = [];
 
     // For each place, get the icon, name and location.
-    let bounds = new google.maps.LatLngBounds();
+    const bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
       if (!place.geometry) {
         return;
