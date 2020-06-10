b5f3b837e5b8fc0207acf939740f7e9e70d5c8a2
Julia Reichel <jrr2189@barnard.edu>
2020-06-10 19:15:05 +0000

    Got rid of unused code.

--- portfolio/src/main/java/com/google/sps/servlets/DeletePostServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeletePostServlet.java
@@ -7,9 +7,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +25,6 @@ public class DeletePostServlet extends HttpServlet {
     Query query = new Query(FACT_ENTITY);
     PreparedQuery results = datastore.prepare(query);
 
-    List<FactPost> fact_entities = new ArrayList<>();
     for (Entity entityKey : results.asIterable()){
       datastore.delete(entityKey.getKey());
     } 
